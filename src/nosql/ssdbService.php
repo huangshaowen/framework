01f3c62d4967c62cd8ab8b201f647cc75d713840
huangshaowen <40477536@qq.com>
2020-07-01 09:45:18 +0800

    修改SSDB锁，支持阻塞

--- src/nosql/ssdbService.php
+++ src/nosql/ssdbService.php
@@ -134,6 +134,70 @@ class ssdbService {
     }
 
     /**
+     * 获得锁,如果锁被占用,阻塞,直到获得锁或者超时。
+     * -- 1、如果 $timeout 参数为 0,则立即返回锁。
+     * -- 2、建议 $timeout 设置为 0,避免 redis 因为阻塞导致性能下降。请根据实际需求进行设置。
+     * @param string $cache_id      锁KEY
+     * @param int $lock_second      锁定时间(秒)
+     * @param int $timeout          取锁超时时间(秒)。等于0,如果当前锁被占用,则立即返回失败。如果大于0,则反复尝试获取锁直到达到该超时时间。
+     * @param int $sleep            取锁间隔时间。单位(微秒)。当锁为占用状态时。每隔多久尝试去取锁。默认 0.1 秒一次取锁。
+     * @return boolean              值或false
+     */
+    public function lock(string $cache_id, int $lock_second = 5, int $timeout = 0, int $sleep = 100000) {
+        $key = "lock:{$cache_id}";
+
+        if (!is_int($timeout) || $timeout < 0) {
+            $timeout = 0;
+        }
+
+        if ($lock_second <= 0) {
+            $lock_second = 1;
+        }
+
+        $start = $this->getMicroTime();
+
+        $lock_value = time();
+
+        do {
+            $rs = $this->_getConForKey($key)->setx($key, $lock_value, $lock_second);
+            if ($rs) {
+                break;
+            }
+            if ($timeout === 0) {
+                break;
+            }
+            usleep($sleep);
+        } while (($this->getMicroTime()) < ($start + ($timeout * 1000000)));
+        return $rs ? $lock_value : false;
+    }
+
+    /**
+     * 对指定键名移除锁标记
+     * @param string    $cache_id       锁名称
+     * @param int       $lock_value     值
+     * @return boolean
+     */
+    public function unlock($cache_id, $lock_value) {
+        $key = "lock:{$cache_id}";
+
+        $rs = $this->_getConForKey($key)->get($key);
+        if ($rs && ($rs == $lock_value)) {
+            return $this->_getConForKey($key)->del($key);
+        }
+
+        return false;
+    }
+
+    /**
+     * 获取当前微秒。
+     *
+     * @return bigint
+     */
+    private function getMicroTime() {
+        return bcmul(microtime(true), 1000000);
+    }
+
+    /**
      * 设置指定 key 的值内容.
      * 参数
      *      key
